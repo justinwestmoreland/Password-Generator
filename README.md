cd1a98ef8f8c15ae267c4d61d149e7fc24535c50
Justin Westmoreland <justinrwestmoreland@gmail.com>
2021-01-14 20:09:02 -0700

    added picture to readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Password-Generator
+
+(passwordgenerator.png)
+
--- newscript.js
+++ newscript.js
@@ -4,11 +4,12 @@ var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var specials = [' ', '!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', '"', "'"];
-var oneArray = [];
 
-// Write password to the #password input
 function writePassword() {
-  // prompt user for a password length, do things if it's outside 8 and 128
+  //this loop promopts people to enter a password length of their choosing. If password is fewer than 8 or greater 
+  //than 128 the function is stopped and restarted. 
+
+  var oneArray = [];
   var passwordLength = window.prompt("Select Length of Password between 8 and 128 characters");
   if (passwordLength === null) {
     return;
@@ -17,21 +18,22 @@ function writePassword() {
     alert("Please select a value between 8 and 128");
     return writePassword();
   }
-  if (passwordLength >= 8 && passwordLength <= 128) {
-  alert("Now you'll chooose which characters you'll want to use.")    
-  }
   if (passwordLength < 8 || passwordLength > 128) {
-      alert("Please select a value between 8 and 128");
-      return writePassword();
+    alert("Please select a value between 8 and 128");
+    return writePassword();
   }
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    alert("Now you'll chooose which characters you'll want to use.")
+  }
+
   // prompt to choose lowercase Y or N
   var lowerCaseChoice = window.confirm("Use Lower Case Letters? OK = Yes or Cancel = No");
   if (lowerCaseChoice === true) {
     oneArray = oneArray.concat(lowerCase);
-    console.log ("I'm going to use Lowercase Letters");
+    console.log("I'm going to use Lowercase Letters");
     console.log(oneArray);
   }
-  else{}
+  else { }
 
   // uppercase Y or N
   var upperCaseChoice = window.confirm("Use Upper Case Letters? OK = Yes or Cancel = No");
@@ -66,25 +68,17 @@ function writePassword() {
     alert("Please select at least one character set");
     writePassword();
   }
- console.log("oneArray", oneArray);
+  console.log("oneArray", oneArray);
 
-// for (var i = 0; i < 50; i++){
-//   var practice = Math.floor(Math.random() * 50);
-//   console.log("practice", practice);
-
-// }
-
-
-  var generatePassword = function(){
+  var generatePassword = function () {
     var results = []
-    for (var i = 0; i < passwordLength; i++){
-    var randomNumber = Math.floor(Math.random() * oneArray.length);
-    // console.log(oneArray[randomNumber]);
-    results.push(oneArray[randomNumber]);
+    for (var i = 0; i < passwordLength; i++) {
+      var randomNumber = Math.floor(Math.random() * oneArray.length);
+      // console.log(oneArray[randomNumber]);
+      results.push(oneArray[randomNumber]);
     }
-  
-  return results.join("");
 
+    return results.join("");
   }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -92,11 +86,11 @@ function writePassword() {
   passwordText.value = password;
 
 
-  
+  for (var i = 0; i < 50; i++){
+    var practice = Math.floor(Math.random() * 50);
+    console.log("practice", practice);
 
-
-
-
+  }
 }
 
 // Add event listener to generate button
