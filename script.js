270e7883dfe17ca586b05b6f44c7f250e541372a
Justin Westmoreland <justinrwestmoreland@gmail.com>
2021-01-16 09:38:48 -0700

    updated comments and removed console.logs

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// Character sets to be used when user selects they want to use an option. 
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var specials = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', '"', "'"];
 
 function writePassword() {
-  //this loop promopts people to enter a password length of their choosing. If password is fewer than 8 or greater 
-  //than 128 characters the function is stopped and restarted. If the user selects cancel at any time, the function is cancelled. 
+  // This loop promopts people to enter a password length of their choosing. If password is fewer than 8 or greater 
+  // than 128 characters the function is stopped and restarted. If the user selects cancel at any time, the function is cancelled. 
+  
+  // oneArray begins blank and will be the array all user selections are concatenated. 
   var oneArray = [];
   var passwordLength = window.prompt("Select Length of Password between 8 and 128 characters");
   if (passwordLength === null) {
@@ -27,13 +31,14 @@ function writePassword() {
     alert("Please select a value between 8 and 128");
     return writePassword();
   }
-  // prompt to choose lowercase Y or N
+  
+  // Users are prompted to select whether or not to include lowercase, uppercase, numbers, or special characters for lines 31 through
+  // 89. When users do not select a Y or N, they are sent back to the beginning to begin again. If a user selects Y for any of the characters
+  // all possible choices are put into oneArray to be stored to use when generating a random password. 
   var lowerCaseChoice = window.prompt("Use Lower Case Letters? Y or N");
   lowerCaseChoice = lowerCaseChoice.toUpperCase();
   if (lowerCaseChoice === "Y") {
     oneArray = oneArray.concat(lowerCase);
-    console.log("I'm going to use Lowercase Letters");
-    console.log(oneArray);
   }
   else if (lowerCaseChoice === "N") {
   }
@@ -41,14 +46,12 @@ function writePassword() {
     alert("Please select Y or N");
     return writePassword();
   }
-  // uppercase Y or N
+ 
   var upperCaseChoice = window.prompt("Use Upper Case Letters? Y or N");
   upperCaseChoice = upperCaseChoice.toUpperCase();
   if (upperCaseChoice === "Y") {
-    console.log("I'm going to use uppercase letters");
     oneArray = oneArray.concat(upperCase);
-    console.log(oneArray);
-    //pull random number of uppercase letters
+  
   }
   else if (upperCaseChoice === "N") {
   }
@@ -56,14 +59,12 @@ function writePassword() {
     alert("Please select Y or N");
     return writePassword();
   }
-  // numeric Y or N
+  
   var numbersChoice = window.prompt("Use Numbers? Y or N");
   numbersChoice = numbersChoice.toUpperCase();
   if (numbersChoice === "Y") {
-    console.log("I'm going to use Numbers");
     oneArray = oneArray.concat(numbers);
-    console.log(oneArray);
-    //pull random numners of numerical characters
+  
   }
   else if (numbersChoice === "N") {
   }
@@ -71,13 +72,11 @@ function writePassword() {
     alert("Please select Y or N");
     return writePassword();
   }
-  // special characters Y or N
+  
   var specialsChoice = window.prompt("Use Special Characters? Y or N");
   specialsChoice = specialsChoice.toUpperCase();
   if (specialsChoice === "Y") {
-    console.log("I'm going to use special characters");
     oneArray = oneArray.concat(specials);
-    console.log(oneArray);
   }
   else if (specialsChoice === "N") {
   }
@@ -85,17 +84,19 @@ function writePassword() {
     alert("Please select Y or N");
     return writePassword();
   }
-  // if no character types are selected, end function 
+  // If the user does not select one of the character choices, they recieve an alert that they have to select at least one
+  // and are brought back to the beginning to start again. 
   if (lowerCaseChoice === "N" && upperCaseChoice === "N" && numbersChoice === "N" && specialsChoice === "N") {
     alert("Please select at least one character set");
     return writePassword();
   }
 
+  // This function utilizes the blank array results to house the correct number of random user selected character sets by using a for
+  // loop by pulling random characters from the array oneArray
   var generatePassword = function () {
     var results = []
     for (var i = 0; i < passwordLength; i++) {
       var randomNumber = Math.floor(Math.random() * oneArray.length);
-      // console.log(oneArray[randomNumber]);
       results.push(oneArray[randomNumber]);
     }
     return results.join("");
